c07dbd02ad3f85cb9503c0e7e88c6cfb8bda1e04
Javier Dev <ja.acevedob1@uniandes.edu.co>
2023-11-03 23:44:58 -0500

    Feat : Creacion de header, footer y creacion modelo vehiculo, configuracion de la ruta

--- ventaAutos/src/app/app.module.ts
+++ ventaAutos/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { VehiculoComponent } from './vehiculo/vehiculo.component';
+import { FooterComponent } from './layout/footer/footer.component'
+import { HeaderComponent } from './layout/header/header.component'
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      VehiculoComponent,
+      FooterComponent,
+      HeaderComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: '**', redirectTo: '/', pathMatch: 'full' },
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
